d57a20d21944bac34f90e37e73c3179a50a861a3
Bartosz Malkowski <bmalkowski@tigase.pl>
2009-05-18 19:47:00 +0000

    Improve UID generator
    git-svn-id: file:///home/svn/repos/tigase-pubsub/trunk@214 ea752f3b-7626-48e1-ae42-c030457b2254

--- src/main/java/tigase/pubsub/Utils.java
+++ src/main/java/tigase/pubsub/Utils.java
@@ -21,6 +21,7 @@
  */
 package tigase.pubsub;
 
+import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.logging.Logger;
 
@@ -28,23 +29,117 @@ import tigase.util.JIDUtils;
 
 public class Utils {
 
-	private final static String CHARSET = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static class Spi {
+
+		private static final long serialVersionUID = 6772408791281461866L;
+
+		private int counter;
+
+		private byte n = 0;
+
+		private byte[] P = { (byte) 0x76, (byte) 0xae, (byte) 0x60, (byte) 0xfa, (byte) 0x97, (byte) 0x07, (byte) 0x12,
+				(byte) 0x2d, (byte) 0x72, (byte) 0xe6, (byte) 0xc2, (byte) 0xb1, (byte) 0x0f, (byte) 0xe0, (byte) 0xc3,
+				(byte) 0x3e, (byte) 0xc4, (byte) 0x30, (byte) 0xbf, (byte) 0xc8, (byte) 0x66, (byte) 0xe9, (byte) 0x79,
+				(byte) 0xe3, (byte) 0xcc, (byte) 0x35, (byte) 0xb5, (byte) 0x54, (byte) 0x68, (byte) 0xd0, (byte) 0x4c,
+				(byte) 0x9b, (byte) 0xff, (byte) 0x14, (byte) 0x21, (byte) 0x32, (byte) 0x78, (byte) 0x08, (byte) 0xbd,
+				(byte) 0x63, (byte) 0x13, (byte) 0x47, (byte) 0xd5, (byte) 0x2a, (byte) 0xd3, (byte) 0xca, (byte) 0xdc,
+				(byte) 0x6d, (byte) 0x38, (byte) 0xd9, (byte) 0xf9, (byte) 0x34, (byte) 0x0e, (byte) 0x73, (byte) 0x82,
+				(byte) 0x46, (byte) 0x04, (byte) 0x62, (byte) 0xa2, (byte) 0xe8, (byte) 0xc9, (byte) 0x4f, (byte) 0xb2,
+				(byte) 0xb3, (byte) 0xb6, (byte) 0x99, (byte) 0x2b, (byte) 0x7c, (byte) 0x37, (byte) 0x96, (byte) 0x7f,
+				(byte) 0x8c, (byte) 0x22, (byte) 0x5f, (byte) 0x77, (byte) 0xf7, (byte) 0xe5, (byte) 0x17, (byte) 0xed,
+				(byte) 0xe1, (byte) 0x6b, (byte) 0x8a, (byte) 0xf6, (byte) 0x80, (byte) 0xd1, (byte) 0x7d, (byte) 0xb9,
+				(byte) 0x36, (byte) 0x05, (byte) 0xdf, (byte) 0x6c, (byte) 0x3a, (byte) 0x51, (byte) 0xea, (byte) 0x57,
+				(byte) 0x94, (byte) 0x88, (byte) 0x06, (byte) 0xac, (byte) 0xc5, (byte) 0x2c, (byte) 0x11, (byte) 0xba,
+				(byte) 0xcd, (byte) 0x9a, (byte) 0x2f, (byte) 0x20, (byte) 0xbb, (byte) 0x9f, (byte) 0xef, (byte) 0x8e,
+				(byte) 0x71, (byte) 0x6e, (byte) 0x1b, (byte) 0x84, (byte) 0xb0, (byte) 0xcf, (byte) 0x28, (byte) 0xf2,
+				(byte) 0xab, (byte) 0xd6, (byte) 0x16, (byte) 0x45, (byte) 0xa7, (byte) 0x83, (byte) 0x01, (byte) 0x02,
+				(byte) 0x2e, (byte) 0x3b, (byte) 0x75, (byte) 0x9e, (byte) 0xa0, (byte) 0xa9, (byte) 0xa1, (byte) 0x7e,
+				(byte) 0xcb, (byte) 0x74, (byte) 0x81, (byte) 0x91, (byte) 0x0b, (byte) 0x86, (byte) 0x44, (byte) 0x42,
+				(byte) 0x3f, (byte) 0x15, (byte) 0x24, (byte) 0x31, (byte) 0x69, (byte) 0xfd, (byte) 0xce, (byte) 0x7a,
+				(byte) 0xe2, (byte) 0x1c, (byte) 0xb4, (byte) 0x8b, (byte) 0x1f, (byte) 0x6a, (byte) 0x65, (byte) 0x7b,
+				(byte) 0xe4, (byte) 0xf4, (byte) 0xf5, (byte) 0xde, (byte) 0x18, (byte) 0xfb, (byte) 0xdd, (byte) 0xf0,
+				(byte) 0x43, (byte) 0x09, (byte) 0x53, (byte) 0xd4, (byte) 0x92, (byte) 0x8f, (byte) 0x9d, (byte) 0x9c,
+				(byte) 0x59, (byte) 0x00, (byte) 0x3d, (byte) 0xa3, (byte) 0x41, (byte) 0xdb, (byte) 0x55, (byte) 0x95,
+				(byte) 0x40, (byte) 0xc7, (byte) 0x49, (byte) 0xfc, (byte) 0x0a, (byte) 0xa4, (byte) 0x10, (byte) 0x64,
+				(byte) 0x6f, (byte) 0x4b, (byte) 0xda, (byte) 0xeb, (byte) 0xee, (byte) 0xb8, (byte) 0xa8, (byte) 0xec,
+				(byte) 0xbe, (byte) 0x5e, (byte) 0xc0, (byte) 0xc1, (byte) 0x4e, (byte) 0x33, (byte) 0x52, (byte) 0x58,
+				(byte) 0x0d, (byte) 0x98, (byte) 0x90, (byte) 0xbc, (byte) 0x25, (byte) 0xfe, (byte) 0x48, (byte) 0x5d,
+				(byte) 0xd8, (byte) 0x23, (byte) 0x03, (byte) 0x3c, (byte) 0x39, (byte) 0x87, (byte) 0xf8, (byte) 0x93,
+				(byte) 0xe7, (byte) 0xd2, (byte) 0x5b, (byte) 0xf3, (byte) 0x29, (byte) 0x61, (byte) 0xd7, (byte) 0x1e,
+				(byte) 0x27, (byte) 0x85, (byte) 0x89, (byte) 0x50, (byte) 0x67, (byte) 0xb7, (byte) 0x56, (byte) 0xaf,
+				(byte) 0x4d, (byte) 0x0c, (byte) 0xa6, (byte) 0x1d, (byte) 0x4a, (byte) 0x70, (byte) 0xad, (byte) 0xa5,
+				(byte) 0x8d, (byte) 0xf1, (byte) 0x19, (byte) 0x1a, (byte) 0xaa, (byte) 0xc6, (byte) 0x5c, (byte) 0x26,
+				(byte) 0x5a };
+
+		private final SecureRandom random = new SecureRandom();
+
+		private byte s = (byte) 0xde;
+
+		public Spi() {
+			long seed = System.nanoTime();
+			byte[] s = new byte[4];
+			s[3] = (byte) (seed & 0x000000ff);
+			s[2] = (byte) ((seed & 0x0000ff00) >> 8);
+			s[1] = (byte) ((seed & 0x00ff0000) >> 16);
+			s[0] = (byte) ((seed & 0xff000000) >> 24);
+			engineSetSeed(s);
+
+			byte[] s1 = random.generateSeed(768);
+			engineSetSeed(s1);
+
+		}
+
+		protected byte[] engineGenerateSeed(int numBytes) {
+			byte[] result = new byte[numBytes];
+			engineNextBytes(result);
+			return result;
+		}
+
+		public void engineNextBytes(byte[] bytes) {
+			engineNextBytes(bytes, 0);
+		}
+
+		public void engineNextBytes(byte[] bytes, int offset) {
+			for (int i = offset; i != bytes.length; i++) {
+				s = P[(s + P[n & 0xff]) & 0xff];
+				bytes[i] = P[(P[(P[s & 0xff]) & 0xff] + 1) & 0xff];
+				byte temp = P[n & 0xff];
+				P[n & 0xff] = P[s & 0xff];
+				P[s & 0xff] = temp;
+				n = (byte) ((n + 1) & 0xff);
+				++counter;
+			}
+			if (counter > 16401001) {
+				counter = 0;
+				byte[] s1 = random.generateSeed(768);
+				engineSetSeed(s1);
+			}
+		}
+
+		public void engineSetSeed(byte[] seed) {
+			if (seed != null && P != null) {
+				for (int m = 0; m < seed.length; m++) {
+					s = P[(s + P[n & 0xff] + seed[m]) & 0xff];
+					byte temp = P[n & 0xff];
+					P[n & 0xff] = P[s & 0xff];
+					P[s & 0xff] = temp;
+					n = (byte) ((n + 1) & 0xff);
+				}
+			}
+		}
+
+		public void updateSeed(int hashCode) {
+			s = P[(s + P[n & 0xff] + hashCode) & 0xff];
+			byte temp = P[n & 0xff];
+			P[n & 0xff] = P[s & 0xff];
+			P[s & 0xff] = temp;
+			n = (byte) ((n + 1) & 0xff);
+		}
+	}
 
 	protected static Logger log = Logger.getLogger(Utils.class.getName());
 
-	private static SecureRandom numberGenerator;
-
-	public static String longToTime(long time) {
-		long hours = time / 3600000;
-		long mins = (time - (hours * 3600000)) / 60000;
-		long secs = (time - ((hours * 3600000) + (mins * 60000))) /	1000;
-		long millis = (time - ((hours * 3600000) + (mins * 60000) + secs * 1000));
-		return "" + 
-						(hours > 0 ? hours + "h, " : "") +
-						(mins > 0 ? mins + "m, " : "") +
-						(secs > 0 ? secs + "sec, " : "") +
-						(millis > 0 ? millis + "ms" : "");
-	}
+	private final static Spi spi = new Spi();
 
 	public static String asString(String... array) {
 		StringBuilder sb = new StringBuilder();
@@ -70,24 +165,18 @@ public class Utils {
 		return false;
 	}
 
-	// This is extremely slow and to make things worse it is synchronized
-//	public static synchronized String createUID() {
-//		SecureRandom ng = numberGenerator;
-//		if (ng == null) {
-//			numberGenerator = ng = new SecureRandom();
-//		}
-//		String result = "";
-//		for (int i = 0; i < 16; i++) {
-//			int a = ng.nextInt(CHARSET.length());
-//			result += CHARSET.charAt(a);
-//		}
-//		return result;
-//	}
-
 	public static String createUID(String jid) {
-		StringBuilder sb = new StringBuilder(jid);
-		sb.append(sb.hashCode());
-		return "" + Math.abs(sb.toString().hashCode());
+		byte[] x = new byte[13];
+
+		spi.engineNextBytes(x, 1);
+		if (jid != null)
+			spi.updateSeed(jid.hashCode());
+
+		return (new BigInteger(x)).toString(36);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(createUID("null"));
 	}
 
 	public static boolean isAllowedDomain(final String jid, final String... domains) {
@@ -100,6 +189,15 @@ public class Utils {
 				return true;
 		}
 		return false;
+	}
+
+	public static String longToTime(long time) {
+		long hours = time / 3600000;
+		long mins = (time - (hours * 3600000)) / 60000;
+		long secs = (time - ((hours * 3600000) + (mins * 60000))) / 1000;
+		long millis = (time - ((hours * 3600000) + (mins * 60000) + secs * 1000));
+		return "" + (hours > 0 ? hours + "h, " : "") + (mins > 0 ? mins + "m, " : "") + (secs > 0 ? secs + "sec, " : "")
+				+ (millis > 0 ? millis + "ms" : "");
 	}
 
 	private Utils() {
