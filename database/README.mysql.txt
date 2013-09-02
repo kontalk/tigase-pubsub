84491f351e9b7761a39ef9496c62c2fdeaa7b1a0
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2013-09-02 14:24:14 +0200

    add support for MS SQL Server to PubSub component; #1461: SQL Server Support - code development

--- database/README.mysql.txt
+++
@@ -1,5 +0,0 @@
-In order to use dedicated PubSub tables, you need to:
-1. import mysql-schema.sql to Tigase MySQL DB
-2. set Tigase configuration:
-  pubsub/pubsub-repo-class = tigase.pubsub.repository.PubSubDAOJDBC
-  pubsub/pubsub-repo-url = "database connection string, usually the same as for user repo"
--- src/main/java/tigase/pubsub/repository/PubSubDAOJDBC.java
+++ src/main/java/tigase/pubsub/repository/PubSubDAOJDBC.java
@@ -147,7 +147,16 @@ public class PubSubDAOJDBC extends PubSubDAO {
 				create_node_sp.setInt(3, nodeType.ordinal());
 				create_node_sp.setString(4, ownerJid.toString());
 				create_node_sp.setString(5, serializedNodeConfig);
+
+				if ( db_conn != null ){
+					if ( db_conn.contains( "mysql" ) ){
 				rs = create_node_sp.executeQuery();
+			}
+					if ( db_conn.contains( "sqlserver" ) ){
+						create_node_sp.executeUpdate();
+						rs = create_node_sp.getGeneratedKeys();
+					}
+				}
 			}
 		} catch (SQLIntegrityConstraintViolationException e) {
 			throw new RepositoryException("Error while adding node to repository, already exists?", e);
@@ -525,7 +534,15 @@ public class PubSubDAOJDBC extends PubSubDAO {
 				set_node_affiliations_sp.setString(1, serviceJid.toString());
 				set_node_affiliations_sp.setString(2, nodeName);
 				set_node_affiliations_sp.setString(3, serializedData);
+				if ( db_conn != null ){
+					if ( db_conn.contains( "mysql" ) ){
 				rs = set_node_affiliations_sp.executeQuery();
+			}
+					if ( db_conn.contains( "sqlserver" ) ){
+						set_node_affiliations_sp.executeUpdate();
+						rs = set_node_affiliations_sp.getGeneratedKeys();
+					}
+				}
 			}
 		} catch (SQLException e) {
 			throw new RepositoryException("Node subscribers writing error", e);
@@ -544,7 +561,15 @@ public class PubSubDAOJDBC extends PubSubDAO {
 				set_node_configuration_sp.setString(1, serviceJid.toString());
 				set_node_configuration_sp.setString(1, nodeName);
 				set_node_configuration_sp.setString(2, serializedData);
+				if ( db_conn != null ){
+					if ( db_conn.contains( "mysql" ) ){
 				rs = set_node_configuration_sp.executeQuery();
+			}
+					if ( db_conn.contains( "sqlserver" ) ){
+						set_node_configuration_sp.executeUpdate();
+						rs = set_node_configuration_sp.getGeneratedKeys();
+					}
+				}
 			}
 		} catch (SQLException e) {
 			throw new RepositoryException("Node configuration writing error", e);
@@ -564,7 +589,15 @@ public class PubSubDAOJDBC extends PubSubDAO {
 				set_node_subscriptions_sp.setString(2, nodeName);
 				set_node_subscriptions_sp.setInt(3, changedIndex);
 				set_node_subscriptions_sp.setString(4, serializedData);
+				if ( db_conn != null ){
+					if ( db_conn.contains( "mysql" ) ){
 				rs = set_node_subscriptions_sp.executeQuery();
+			}
+					if ( db_conn.contains( "sqlserver" ) ){
+						set_node_subscriptions_sp.executeUpdate();
+						rs = set_node_subscriptions_sp.getGeneratedKeys();
+					}
+				}
 			}
 		} catch (SQLException e) {
 			throw new RepositoryException("Node subscribers writing error", e);
@@ -585,7 +618,15 @@ public class PubSubDAOJDBC extends PubSubDAO {
 				write_item_sp.setString(3, id);
 				write_item_sp.setString(4, publisher);
 				write_item_sp.setString(5, item.toString());
+				if ( db_conn != null ){
+					if ( db_conn.contains( "mysql" ) ){
 				rs = write_item_sp.executeQuery();
+			}
+					if ( db_conn.contains( "sqlserver" ) ){
+						write_item_sp.executeUpdate();
+						rs = write_item_sp.getGeneratedKeys();
+					}
+				}
 			}
 		} catch (SQLException e) {
 			throw new RepositoryException("Item writing error", e);
