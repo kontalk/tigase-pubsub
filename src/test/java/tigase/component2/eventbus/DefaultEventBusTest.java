44f8cd29242e3fe3876d16004bbaa02249c7adf2
Bartosz Malkowski <bmalkowski@tigase.pl>
2013-09-17 12:03:14 +0200

    change component package in tests

--- src/test/java/tigase/component2/eventbus/DefaultEventBusTest.java
+++ src/test/java/tigase/component2/eventbus/DefaultEventBusTest.java
@@ -1,17 +1,17 @@
-package tigase.component.eventbus;
+package tigase.component2.eventbus;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import tigase.component.eventbus.DefaultEventBusTest.Test01Handler.Test01Event;
-import tigase.component.eventbus.DefaultEventBusTest.Test02Handler.Test02Event;
 import tigase.component2.eventbus.DefaultEventBus;
 import tigase.component2.eventbus.Event;
 import tigase.component2.eventbus.EventBus;
 import tigase.component2.eventbus.EventHandler;
 import tigase.component2.eventbus.EventListener;
 import tigase.component2.eventbus.EventType;
+import tigase.component2.eventbus.DefaultEventBusTest.Test01Handler.Test01Event;
+import tigase.component2.eventbus.DefaultEventBusTest.Test02Handler.Test02Event;
 
 public class DefaultEventBusTest {
 
