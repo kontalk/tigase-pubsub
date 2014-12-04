3c2672b64e37fbd695170b3e99b8cf8af8d6b5eb
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2014-12-04 13:56:12 +0100

    extend ad-hoc RetrieveItemsCommand script with option to retrieve particular item; #2474

--- src/main/java/tigase/pubsub/modules/commands/RetrieveItemsCommand.java
+++ src/main/java/tigase/pubsub/modules/commands/RetrieveItemsCommand.java
@@ -1,13 +1,14 @@
 package tigase.pubsub.modules.commands;
 
-import java.util.Calendar;
-import java.util.Date;
+import tigase.db.UserRepository;
+
+import tigase.xmpp.Authorization;
+import tigase.xmpp.BareJID;
 
 import tigase.adhoc.AdHocCommand;
 import tigase.adhoc.AdHocCommandException;
 import tigase.adhoc.AdHocResponse;
 import tigase.adhoc.AdhHocRequest;
-import tigase.db.UserRepository;
 import tigase.form.Field;
 import tigase.form.Form;
 import tigase.pubsub.PubSubConfig;
@@ -15,7 +16,10 @@ import tigase.pubsub.repository.IItems;
 import tigase.pubsub.repository.IPubSubRepository;
 import tigase.util.DateTimeFormatter;
 import tigase.xml.Element;
-import tigase.xmpp.Authorization;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 public class RetrieveItemsCommand implements AdHocCommand {
 
@@ -26,6 +30,12 @@ public class RetrieveItemsCommand implements AdHocCommand {
 	private final IPubSubRepository repository;
 
 	private final UserRepository userRepo;
+
+	public static final String TIGASE_PUBSUB_SERVICE_KEY = "tigase-pubsub#service-name";
+	public static final String TIGASE_PUBSUB_NODENAME_KEY = "tigase-pubsub#node-name";
+	public static final String TIGASE_PUBSUB_TIMESTAMP_KEY = "tigase-pubsub#timestamp";
+	public static final String TIGASE_PUBSUB_ITEMID_KEY = "tigase-pubsub#item-id";
+	public static final String TIGASE_PUBSUB_INTERNAL_KEY = "tigase-pubsub#internal";
 
 	/**
 	 * Constructs ...
@@ -65,9 +75,12 @@ public class RetrieveItemsCommand implements AdHocCommand {
 				if (data == null) {
 					Form form = new Form("result", "Retrieve items", null);
 
-					form.addField(Field.fieldTextSingle("tigase-pubsub#node-name", "", "Node name"));
-					form.addField(Field.fieldTextSingle("tigase-pubsub#timestamp", dtf.formatDateTime(new Date()),
+					form.addField(Field.fieldTextSingle(TIGASE_PUBSUB_SERVICE_KEY, "", "Service name"));
+					form.addField(Field.fieldTextSingle(TIGASE_PUBSUB_NODENAME_KEY, "", "Node name"));
+					form.addField(Field.fieldTextSingle(TIGASE_PUBSUB_ITEMID_KEY, "", "Item ID"));
+					form.addField(Field.fieldTextSingle(TIGASE_PUBSUB_TIMESTAMP_KEY, dtf.formatDateTime(new Date()),
 							"Items since"));
+					form.addField(Field.fieldHidden( TIGASE_PUBSUB_INTERNAL_KEY, "" ));
 
 					response.getElements().add(form.getElement());
 					response.startSession();
@@ -76,38 +89,61 @@ public class RetrieveItemsCommand implements AdHocCommand {
 					if ("submit".equals(form.getType())) {
 						final Boolean rebuild = form.getAsBoolean("tigase-pubsub#delete-all");
 
-						String nodeName = form.getAsString("tigase-pubsub#node-name");
-						String timeStr = form.getAsString("tigase-pubsub#timestamp");
+						String serviceName = form.getAsString(TIGASE_PUBSUB_SERVICE_KEY);
+						String nodeName = form.getAsString(TIGASE_PUBSUB_NODENAME_KEY);
+						String nodeId = form.getAsString(TIGASE_PUBSUB_ITEMID_KEY);
+						String timeStr = form.getAsString(TIGASE_PUBSUB_TIMESTAMP_KEY);
 						final Calendar timestamp = timeStr == null || timeStr.trim().length() == 0 ? null
 								: dtf.parseDateTime(timeStr);
+						String internalId = form.getAsString(TIGASE_PUBSUB_INTERNAL_KEY);
 
 						if (nodeName == null) {
 							throw new AdHocCommandException(Authorization.BAD_REQUEST, "Empty node name.");
-						} else if (timestamp == null) {
+						} else if (timestamp == null && nodeId == null) {
 							throw new AdHocCommandException(Authorization.BAD_REQUEST, "Invalid timestamp.");
 						}
 
 						// ==================
 
 						Element f = new Element("x", new String[] { "xmlns" }, new String[] { "jabber:x:data" });
-						f.addChild(new Element("title", "Items"));
-						Element reported = new Element("reported");
-						reported.addChild(new Element("field", new String[] { "var" }, new String[] { "id" }));
-						f.addChild(reported);
+						IItems nodeItems;
+						if ( null != serviceName ){
+							nodeItems = repository.getNodeItems( BareJID.bareJIDInstance( serviceName ), nodeName );
+						} else {
+							f.addChild(new Element("title", "Items"));
+							Element reported = new Element( "reported" );
+							reported.addChild( new Element( "field", new String[] { "var" }, new String[] { "id" } ) );
+							f.addChild( reported );
 
-						IItems nodeItems = repository.getNodeItems(request.getIq().getTo().getBareJID(), nodeName);
-						String[] allItems = nodeItems.getItemsIdsSince(timestamp.getTime());
-						for (String id : allItems) {
-							Element i = new Element("item");
-							Element fi = new Element("field", new String[]{"var"}, new String[]{"id"});
-							fi.addChild(new Element("value", id));
-							i.addChild(fi);
-							f.addChild(i);
+							nodeItems = repository.getNodeItems( request.getIq().getTo().getBareJID(), nodeName );
 						}
-						// ==================
-						response.getElements().add(f);
-					}
+						if ( null != nodeId ){
+							if ( Arrays.asList( nodeItems.getItemsIds() ).contains( nodeId ) ){
+								Element i = nodeItems.getItem( nodeId );
+								Element field = new Element( "field", new String[] { "var" }, new String[] { "item" } );
+								field.addChild( new Element( "value", new Element[] { i }, null, null ) );
 
+								f.addChild( field );
+							}
+						} else {
+							String[] allItems = nodeItems.getItemsIdsSince( timestamp.getTime() );
+							for ( String id : allItems ) {
+								Element i = new Element( "item" );
+								Element fi = new Element( "field", new String[] { "var" }, new String[] { "id" } );
+								fi.addChild( new Element( "value", id ) );
+								i.addChild( fi );
+								f.addChild( i );
+							}
+
+							// ==================
+						}
+
+						if ( null != internalId ){
+							Field fieldHidden = Field.fieldHidden( TIGASE_PUBSUB_INTERNAL_KEY, internalId );
+							f.addChild(fieldHidden.getElement() );
+						}
+						response.getElements().add( f );
+					}
 					response.completeSession();
 				}
 			}
