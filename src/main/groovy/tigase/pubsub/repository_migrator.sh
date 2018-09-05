81ebf6e94a56e91244ea1cfe91d64e8f0b3d97a8
Daniele Ricci <daniele@casaricci.it>
2018-09-05 14:26:05 +0200

    Build against Kontalk Tigase server
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/main/groovy/tigase/pubsub/repository_migrator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LIBS="/home/smoku/workspace/tigase-server/target /home/smoku/workspace/tigase-server/libs /home/smoku/workspace/tigase-pubsub/target /usr/share/tigase-server/lib"
-for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | tr '\n' :`$CLASSPATH"; done
-export CLASSPATH
-#echo $CLASSPATH
-exec ${0/%.sh/.groovy}
